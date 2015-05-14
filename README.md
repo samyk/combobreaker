ebd159dc49177a2e20959ad6ce3ab2e815267cfb
Samy Kamkar <git@samy.pl>
2015-05-14 01:44:19 -0700

    Added code + 3d models

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-# combobreaker
-Motorized, portable, 3D printed, Arduino-based combination lock cracker
+# [Combo Breaker](http://samy.pl/combobreaker/)
+
+**[Combo Breaker](http://samy.pl/combobreaker/)** is a motorized, battery powered, 3D printed, Arduino-based combination lock cracking device. It is portable, open source, schematic and 3D models provided, and exploits a new technique I've discovered for [cracking combination locks in 8 attempts or less](https://www.youtube.com/watch?v=09UgmwtL12c), but in an even more exciting, automated fashion.
+
+**Live demonstration** and full details available in the video:
+<a href="https://www.youtube.com/watch?v=YcpSvHpbHQ4" target="_blank"><img src="http://img.youtube.com/vi/YcpSvHpbHQ4/0.jpg" alt="KeySweeper" width="640" height="480" border="10" /></a>
+
+-----
+
+**Point of Contact:** [@SamyKamkar](https://twitter.com/samykamkar) // <http://samy.pl>
+
+**Video demonstration / explanation:** [https://youtu.be/YcpSvHpbHQ4](https://youtu.be/YcpSvHpbHQ4)
+
+**Released:** May 15, 2015
+
+**Source code / schematic / 3D models:** [https://github.com/samyk/combobreaker](https://github.com/samyk/combobreaker)
+
+**Unit Cost:** < $100
+
+**Status:** Operational, open source, open hardware, declassified.
+
+-----
+
+# (U) Hardware
+
+[![Combo Breaker](http://samy.pl/combobreaker/sIMG_2920.JPG)](http://samy.pl/combobreaker/sIMG_2920.JPG)
+
+### [Arduino Nano](http://amzn.to/1QLlf23)
+**$15**: A [5V Arduino Nano](http://amzn.to/1QLlf23) microcontroller is used as the brains of the project.
+
+### [Allegro A3967 / EasyDriver Stepper Driver](http://amzn.to/1A1Ovxl)
+**$8**: I originally used an A3967-based [EasyDriver](http://amzn.to/1A1Ovxl) which can drive a stepper motor up to 30V at 750mA. This is what precisely controls the dial.
+
+### or [Allegro A4988](http://amzn.to/1L3q7fK)
+**$13**: Instead of the EasyDriver, I upgraded to a smaller, yet more powerful stepper driver that can go up to 1 amp (1000mA).
+
+### [Stepper Motor](http://amzn.to/1FfLUQG)
+**$18**: You can use a normal stepper motor for the "simpler" version of the Combo Breaker build, however if you're working on creating the more advanced version, you'll need a double-shaft stepper motor like I liste below.
+
+### or [Double Shaft Stepper Motor](http://www.phidgets.com/products.php?product_id=3320)
+**$16**: This double shaft stepper motor allows you to build the more advanced Combo Breaker while employing an optical rotary encoder on the back to detect the position of the stepper when the dial stops turning (which is part of the exploit employed).
+
+### [HKT22 Optical Rotary Encoder](http://www.phidgets.com/products.php?product_id=3531)
+**$25**: For use **only** with the double shaft stepper motor, this optical rotary encoder lets us know what position the shaft of the stepper motor is at and allows us to employ an exploit on Master combination locks where the stepper will get locked into certain grooves within the internal combination disc of the lock.
+
+### [Analog Feedback Servo](https://www.adafruit.com/products/1404)
+**$15**: This servo not only provides the torque we need to lift the shackle (90.26 oz*in), but has an additional wire providing analog feedback that we use to detect the state of the shackle and whether it has opened or not.
+
+### [L7805 Voltage Regulators](http://amzn.to/1A28XOt)
+**$5**: If you're using the analog feedback servo, you'll want to use an external voltage regulator to provide enough juice to the servo. While the Arduino is 5V and can drive the servo normally, it won't be able to pump out enough current for the servo when it starts hitting the shackle and could damage your Arduino.
+
+### [500mAh 3S Lipo Battery](http://amzn.to/1PkRVlz)
+**$14**: This 500mAh 3S (3-cell, 11.1V) battery gives enough juice to the stepper motor to be very effective, and at 500mAh can last between 30-120minutes straight before having to be recharged.
+
+### [Lipo Charger](http://amzn.to/1A1Q2n7)
+**$45**: This lipo charger is overkill if only used for this project, but is a good one that I use at home for a wide range of batteries and cells. You don't need this one, but if you want to recharge the battery, you'll need a charger that supports 3-cell LiPos.
+
+### [Arctic Alumina Thermal Adehsive](http://amzn.to/1PkTakG)
+**$8**: Not required, but this is the thermal **adhesive** I use to **permanently** attach the heatsink to the stepper motor driver. Normal thermal paste has very little adhesion and kept falling off, so I switched from thermal paste to this thermal adhesive.
+
+### [Breadboard and Wires](http://amzn.to/1FfMb6e)
+**$9**: If you don't already have a breadboard and wires, they will kind of help.
+
+[![Combo Breaker Board](http://samy.pl/combobreaker/sIMG_2916.JPG)](http://samy.pl/combobreaker/sIMG_2916.JPG)
+
+-----
+
+# (U) Software
+
+### Combo Breaker
+Combo Breaker's source code can be obtained in entirety from my github: <https://github.com/samyk/combobreaker>
+
+[![Combo Breaker 3D Print](http://samy.pl/combobreaker/model.jpg)](http://samy.pl/combobreaker/model.jpg)
+
+-----
+
+# (U) 3D Models
+
+I've included the 3D models on my github: <https://github.com/samyk/combobreaker>
+ 
+[![Combo Breaker 3D Print](http://samy.pl/combobreaker/smodel.png)](http://samy.pl/combobreaker/smodel.png)
+
+-----
+
+# (U) Contact
+
+**Point of Contact:** [@SamyKamkar](https://twitter.com/samykamkar)
+
+You can see more of my projects at <http://samy.pl>.
+
+Thanks!
+
+------
---
+++ combobreaker.ino
@@ -0,0 +1,380 @@
+/*
+
+ C-C-C-C-Combo Breaker
+ by samy kamkar
+ http://samy.pl/combobreaker
+ 
+ A motorized, battery powered, 3D printed, 
+ Arduino-based combination lock cracking device.
+ 
+ current hardware: 
+ Stepper Motor (dial): 28STH32-0674B (3.8V 0.67A stepper)
+ Rotary Encoder: HKT22-3531 optical rotary encoder (1200steps)
+ Servo Motor (shackle): Batan S1213 (with analog feedback)
+ Stepper Driver: Allegro A3967 or Allegro A4988 (EasyDriver, 750mA, 1/8th step)
+ Microcontroller: Arduino Nano ATmega328P @ 5V
+ 
+ Stepper/Arduino Power: 3S LiPo battery (12V power supply also works)
+ Servo Power: L7805 Voltage Regulator
+ 
+ */
+
+// Servo requirements: at least 6.5kg/cm of torque
+
+// TODO: CW could be autodetected by the rotary encoder
+// Depending on your stepper wiring, you may need to change CW to true or false
+#define CW true // clockwise
+#define CCW !CW // counter-clockwise
+
+// Servo details (lifts the shackle)
+#include <Servo.h>
+#include <Encoder.h>
+
+#define SERVO_DEFAULT 30
+#define SERVO_MIN 0
+#define SERVO_MAX 200
+#define SERVO_PIN 9
+#define SERVO_FEEDBACK_PIN A0
+#define FB_DIFF 10
+#define FB_TOLERANCE 3.5
+#define FB_DELAY 500
+
+// Encoder details (pins)
+#define ENCODER1 2 // 5
+#define ENCODER2 3 // 6
+#define ENCODER_STEPS 1200 // 1200 per rotation
+
+// Stepper driver details (controls the dial)
+#define DIR_PIN  5 // 2
+#define STEP_PIN 6 // 3
+
+
+#define STEPS 200
+#define MICROSTEPS 8 // was 8 on easydriver
+
+// fastest (us) we can get stepper moving without skipping
+#define MIN_DELAY (1600 / MICROSTEPS) // was 200
+
+// Number of digits on a Master combo lock
+#define DIGITS 40
+
+// C's % is remainder instead of modulo
+#define mod(a, b) ((a % b + b) % b)
+
+// For debug
+#define BAUDRATE 9600
+
+// this means we can rotate *consistently* 3.125 times per second (187.5rpm):
+// 1sec in us / min_delay / (steps * microsteps)
+// 1_000_000  / 200       / (200   * 8) = 3.125
+int current_digit = 0;
+
+Servo shackle;
+Encoder myEnc(ENCODER1, ENCODER2);
+//int servo_pos = 0;
+long oldPosition;
+int mini, maxi, minFeedback, maxFeedback;
+
+void setup() 
+{
+  Serial.begin(BAUDRATE);
+
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(SERVO_FEEDBACK_PIN, INPUT);
+  shackle.attach(SERVO_PIN); 
+  oldPosition = myEnc.read();
+
+  calibrateServo();
+}
+
+void loop()
+{
+  brute(-1, -1, -1);
+
+  for (int i = 0; i < ceil(DIGITS/3); i++)
+  {
+    // turn to new position
+    spinto(0, i * 3, CCW);
+
+    // hold shackle high to find left and right
+    shackleHigh();
+    spinto(0, (i-1)*3, CW);
+    //    delay(100);
+
+    Serial.print("spin low ");
+    Serial.print(oldPosition);
+    Serial.print(" ");
+    spinto(0, (i+1)*3, CCW);
+    //    delay(100);
+    Serial.print(oldPosition);
+    Serial.println("");
+
+    //    delay(100);
+    spinto(0, i * 3, CCW);
+
+    shackleMid();
+    delay(2000);
+  }
+
+  //  spinto(1, 0, CW);
+  //  brute(16, -1, 16);
+  //  pin(0, 0, 0);
+  //  openShackle();
+}
+
+// pulls the shackle up
+// NOTE: if you do this, you should immediately
+// detect if the servo actually moved up, and if not,
+// return back to shackleMid(), otherwise you'll be
+// putting stress on the motor and 3d printed case
+void shackleHigh()
+{
+  shackle.write(maxi + (FB_DIFF*(FB_TOLERANCE-1)));
+}
+
+// puts the shackle in middle position
+void shackleMid()
+{
+  shackle.write(maxi);
+}
+
+// attempts to open the shackle *safely*
+boolean openShackle()
+{
+  int fb;
+  boolean ret = false;
+
+  shackle.write(maxi + (FB_DIFF*FB_TOLERANCE*1.5));
+  delay(300);
+
+  fb = analogRead(SERVO_FEEDBACK_PIN);
+  if (fb - maxFeedback > FB_DIFF*FB_TOLERANCE)
+  {
+    Serial.println("OPENED!");
+    ret = true;
+  }
+  else
+  {
+    ret = false;
+    Serial.println("not opened :(");
+  }
+
+  shackleMid();
+  return ret;
+}
+
+// detect the middle and high positions of the servo
+void calibrateServo()
+{
+  int fb;
+  int i = SERVO_DEFAULT;
+
+  shackle.write(i);
+  delay(500);
+  fb = analogRead(SERVO_FEEDBACK_PIN);
+
+  Serial.println(fb);
+  for (i -= FB_DIFF; i > SERVO_MIN; i -= FB_DIFF)
+  {
+    Serial.print("i=");
+    Serial.print(i);
+    shackle.write(i);
+    mini = i;
+    delay(FB_DELAY);
+    minFeedback = analogRead(SERVO_FEEDBACK_PIN);
+    Serial.print(" fb=");
+    Serial.println(minFeedback);
+
+    // if difference is less than 10, we can't move!
+    if (fb - minFeedback < FB_DIFF)
+    {
+      mini = i + (FB_DIFF*FB_TOLERANCE);
+      Serial.println("crap!");
+      break;
+    }
+    fb = minFeedback;
+
+  }
+
+
+  for (i+=FB_DIFF*2; i <= SERVO_MAX; i += FB_DIFF)
+  {
+    Serial.print("i=");
+    Serial.print(i);
+    shackle.write(i);
+    maxi = i;
+    delay(FB_DELAY);
+    maxFeedback = analogRead(SERVO_FEEDBACK_PIN);
+    Serial.print(" fb=");
+    Serial.println(maxFeedback);
+
+    // if difference is less than 10, we can't move!
+    if (maxFeedback - fb < FB_DIFF)
+    {
+      maxi = i - (FB_DIFF*FB_TOLERANCE);
+      Serial.print("crap!! changing to ");
+      Serial.println(maxi);
+      shackleMid();
+      break;
+    }
+    fb = maxFeedback;
+
+  }
+
+  Serial.print("mini=");
+  Serial.print(mini);
+  Serial.print(" maxi=");
+  Serial.print(maxi);
+  Serial.println("");
+}
+
+// brute force pin numbers based off of the
+// mathematical congruencies of the lock
+void brute(int pin1, int pin2, int pin3)
+{
+  int si = pin1, sj = pin2, sk = pin3;
+
+  if (pin1 == -1)
+    si = pin2 >= 0 ?
+    (pin2 + 2) % 4 :
+    pin3 >= 0 ?
+    pin3 % 4 : 0;
+
+  if (pin2 == -1)
+    sj = pin1 >= 0 ?
+    (pin1 + 2) % 4 :
+    pin3 >= 0 ?
+    (pin3 + 2) % 4 : 0;
+
+  if (pin3 == -1)
+    sk = pin1 >= 0 ?
+    pin1 % 4 :
+    pin2 >= 0 ?
+    (pin2 + 2) % 4 : 0;
+
+  Serial.println(si);
+  Serial.println(sj);
+  Serial.println(sk);
+  Serial.println("go");
+
+  for     (int i = si; (pin1 == i || pin1 == -1) && i < DIGITS; i += 4)
+    for   (int j = sj; (pin2 == j || pin2 == -1) && j < DIGITS; j += 4)
+      for (int k = sk; (pin3 == k || pin3 == -1) && k < DIGITS; k += 4)
+      {
+        Serial.print("testing: ");
+        Serial.print(i);
+        Serial.print(" ");
+        Serial.print(j);
+        Serial.print(" ");
+        Serial.print(k);
+        Serial.println();
+
+        pin(i, j, k);
+        if (openShackle())
+          return;
+
+        delay(100);
+      }
+
+}
+
+// enter a full combination through the dial
+void pin(int pin1, int pin2, int pin3)
+{
+  // clockwise 3 times, then to first number  
+  spinto(3, pin1, CW);
+
+  // counterclockwise once, then to second
+  spinto(1, pin2, CCW);
+
+  // clockwise to third number
+  spinto(0, pin3, CW);
+}
+
+// spin to a specific digit,
+// possibly with some additional rotations
+void spinto(int spins, int digit, boolean cw)
+{
+  digitalWrite(DIR_PIN, cw ? HIGH : LOW);
+
+  // spin some amount all the way?
+  for (int i = 0; i < spins; i++)
+  {
+    step(STEPS);
+//    delay(1000);
+  }
+
+  // track where we are for next time (get set later)
+  int tmp = digit;
+  Serial.print("going to ");
+  Serial.print(digit);
+  Serial.print(" cur=");
+  Serial.print(current_digit);
+
+  if (cw == CW)
+    digit = (DIGITS - digit) + current_digit;
+  else
+    digit = digit - current_digit;
+
+  digit = mod(digit, DIGITS);
+
+  // track where we are for next time
+  current_digit = tmp;
+
+  Serial.print(" moving ");
+  Serial.println(digit);
+  step(digit * (STEPS / DIGITS));
+//  delay(1000);
+}
+
+/*
+void rotate(int digits, boolean cw)
+ {
+ rotate(0, digits, cw);
+ }
+ 
+ void rotate(int spins, int digits, boolean cw)
+ {
+ digitalWrite(DIR_PIN, cw ? HIGH : LOW);
+ 
+ // spin some amount all the way?
+ for (int i = 0; i < spins; i++)
+ {
+ step(STEPS);
+// delay(1000);
+ }
+ 
+ 
+ step(digits * (STEPS / DIGITS));
+// delay(1000);
+ }
+ */
+
+// step to location
+// TODO: return encoder feedback
+void step(int steps)
+{
+  long newPosition;
+  for (int i = 0; i < steps * MICROSTEPS; i++)
+  {
+    digitalWrite(STEP_PIN, LOW);
+    digitalWrite(STEP_PIN, HIGH);
+    delayMicroseconds(MIN_DELAY);
+  }
+
+  // get encoder position
+  newPosition = myEnc.read();
+  if (newPosition != oldPosition)
+  {
+    //      oldPosition = newPosition;
+    oldPosition = mod(newPosition, ENCODER_STEPS);//XXX
+    Serial.print("newpos=");
+    Serial.print(newPosition);
+    Serial.print(" actual=");
+    Serial.print((int)(oldPosition / DIGITS));
+    Serial.print(" pos=");
+    Serial.println(oldPosition);
+  }
+}
+
